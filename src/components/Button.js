c1378694378140a73f62e424fc778f5182c55ed2
Shubham Singh <shubhss05@gmail.com>
2020-09-18 14:12:36 +0530

    changes made

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Button extends Component {
+
+    clicked() {
+        alert("Saved as draft");
+      }
+
+    clicked2() {
+        alert ("Form Submitted")
+    }
+
     render() {
         return (
             <div className="row">
@@ -10,11 +19,11 @@ export default class Button extends Component {
                 <div className='col-md-9'>
                     <div className="row">
                         <div className="col-md-6">
-                            <button style={{ marginTop: '40px', backgroundColor: "orange", color: "white", borderRadius: "35px", paddingLeft: '60px', paddingRight: '60px', paddingTop: '10px', paddingBottom: '10px', borderColor: "oran" }}>Save as Draft</button>
+                            <button  onClick = {this.clicked} style={{ marginTop: '40px', backgroundColor: "orange", color: "white", borderRadius: "35px", paddingLeft: '60px', paddingRight: '60px', paddingTop: '10px', paddingBottom: '10px', borderColor: "oran" }}>Save as Draft</button>
 
                         </div>
                         <div className="col-md-6">
-                            <button className="ml-5" style={{ marginTop: '40px', backgroundColor: "orange", color: "white", borderRadius: "35px", paddingLeft: '60px', paddingRight: '60px', paddingTop: '10px', paddingBottom: '10px' }}>Send</button>
+                            <button className="ml-5" onClick = {this.clicked2} style={{ marginTop: '40px', backgroundColor: "orange", color: "white", borderRadius: "35px", paddingLeft: '60px', paddingRight: '60px', paddingTop: '10px', paddingBottom: '10px' }}>Send</button>
 
                         </div>
                         <div className="col-md-6">
